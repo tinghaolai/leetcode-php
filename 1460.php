c5501e06b4084fe260b9a45eab417eeb9e033209
Ting-Hao Lai <analth355@gmail.com>
2022-01-11 01:12:36 +0800

    Add solution for 1460

---
+++ 1460.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $target
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function canBeEqual($target, $arr) {
+        sort($target);
+        sort($arr);
+        return $target === $arr;
+    }
+}
