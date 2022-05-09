142b4c944924738abe400f9a9f17b418857d1686
tinghaolai <analth355@gmail.com>
2022-05-10 03:35:01 +0800

    Test function helper add function: readline

--- TestFunctions.php
+++ TestFunctions.php
@@ -21,3 +21,15 @@ if (!function_exists('dd')) {
         exit();
     }
 }
+
+if(!function_exists('readline')) {
+    function readline($prompt = null){
+        if($prompt){
+            echo $prompt;
+        }
+
+        $fp = fopen('php://stdin', 'r');
+        $line = rtrim(fgets($fp, 1024));
+        return $line;
+    }
+}
