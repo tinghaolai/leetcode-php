55109869893225b439f8a901c200e39ab43ebf5f
tinghaolai <analth355@gmail.com>
2022-05-10 03:35:36 +0800

    Modify test script: auto finding file path

--- test.php
+++ test.php
@@ -1,6 +1,48 @@
 <?php
 
-// php test.php <problem num>
-require $argv[1] . '.php';
+require 'TestFunctions.php';
 
-print(print_r((new Solution())->test(), true));
+if (isset($argv[1])) {
+    $problemNumber = $argv[1];
+} else {
+    $problemNumber = readline('enter problem number: ');
+}
+
+if (substr($problemNumber, -4) !== '.php') {
+    $problemNumber .= '.php';
+}
+
+while (strlen($problemNumber) < 8) {
+    $problemNumber = '0' . $problemNumber;
+}
+
+$require = null;
+foreach (['.', 'easy', 'medium', 'hard']as $folder) {
+    $path = $folder . '/' . $problemNumber;
+    if (@fopen($path, 'r')) {
+        $require = $path;
+        break;
+    }
+}
+
+if (!$require) {
+    dd('Problem not found!');
+}
+
+require $require;
+
+$solution = new Solution();
+if (method_exists($solution,'testAAAAAA')) {
+    dd('--test solution--', $solution->test());
+} else {
+    $firstFunction = get_class_methods($solution)[0];
+    $ref = new ReflectionMethod(Solution::class, $firstFunction);
+    dd(
+        '--test function not found--',
+        'function: ' . $firstFunction,
+        $ref->getDocComment()
+//        ,
+//        $solution->$firstFunction()
+// todo, generate rand input by reading regex of comment, ex: int generates rand number, int[] generates rand numbers of an array.
+    );
+}
