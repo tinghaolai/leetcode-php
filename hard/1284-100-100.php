46c93040c5bafbe3b8f9e287133572ee0c5db219
tinghaolai <analth355@gmail.com>
2022-06-09 23:43:32 +0800

    Update solution for 1284, demonstrate making recursion clearer

--- hard/1284-100-100.php
+++ hard/1284-100-100.php
@@ -7,43 +7,35 @@ class Solution {
     protected $jCount;
     protected $matrixExists = [];
     /**
+     * Runtime: 3368 ms, faster than 100.00% of PHP online submissions for Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.
+    Memory Usage: 20.5 MB, less than 100.00% of PHP online submissions for Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.
+     *
      * @param Integer[][] $mat
      * @return Integer
      */
     function minFlips($mat) {
         $this->zeroMatrix = str_replace('1', '0', json_encode($mat));
-        if (json_encode($mat) === $this->zeroMatrix) {
-            return 0;
-        }
-
         $this->iCount = count($mat);
         $this->jCount = count($mat[0]);
-        $minStep = -1;
-        for ($i = 0; $i < $this->iCount; $i++) {
-            for ($j = 0; $j < $this->jCount; $j++) {
-                $totalStep = $this->flip($mat, $i, $j, 1);
-                if ($totalStep !== -1 && ($minStep === -1 || $totalStep < $minStep )) {
-                    $minStep = $totalStep;
-                }
-            }
-        }
 
-        return $minStep;
+        return $this->flip($mat, null, null, 0, true);
     }
 
-    function flip($matrix, $i, $j, $step)
+    function flip($matrix, $i, $j, $step, $init = false)
     {
-        foreach ([
-                     [0, 0],
-                     [1, 0],
-                     [-1, 0],
-                     [0, 1],
-                     [0, -1],
-                 ] as $offSet) {
-            $flipI = $i + $offSet[0];
-            $flipJ = $j + $offSet[1];
-            if (isset($matrix[$flipI][$flipJ])) {
-                $matrix[$flipI][$flipJ] = $matrix[$flipI][$flipJ] === 0 ? 1 : 0;
+        if (!$init) {
+            foreach ([
+                [0, 0],
+                [1, 0],
+                [-1, 0],
+                [0, 1],
+                [0, -1],
+            ] as $offSet) {
+                $flipI = $i + $offSet[0];
+                $flipJ = $j + $offSet[1];
+                if (isset($matrix[$flipI][$flipJ])) {
+                    $matrix[$flipI][$flipJ] = $matrix[$flipI][$flipJ] === 0 ? 1 : 0;
+                }
             }
         }
 
