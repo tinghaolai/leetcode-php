b20b7d1954ff0817ca82e84defcb22010edaefbd
tinghaolai <analth355@gmail.com>
2023-03-29 18:55:31 +0800

    Update solution for 1402

--- hard/1402-100-100.php
+++ hard/1402-100-100.php
@@ -3,36 +3,22 @@
 class Solution {
 
     /**
-     * Runtime: 21 ms, faster than 100.00% of PHP online submissions for Reducing Dishes.
-    Memory Usage: 19.2 MB, less than 100.00% of PHP online submissions for Reducing Dishes.
-     *
      * @param Integer[] $satisfaction
      * @return Integer
      */
     function maxSatisfaction($satisfaction) {
         rsort($satisfaction);
-        $coefficientTotal = 0;
-        $total = 0;
-        $i = 0;
-        while (
-            $i < count($satisfaction) &&
-            (
-                $satisfaction[$i] >= 0 ||
-                $total > -$satisfaction[$i]
-            )
-        ) {
-            $total += $satisfaction[$i];
-            $coefficientTotal += $total;
-            $i++;
+        $result = 0;
+        $acc = 0;
+        foreach ($satisfaction as $score) {
+            $acc += $score;
+            if ($acc <= 0) {
+                break;
+            }
+
+            $result += $acc;
         }
 
-        return $coefficientTotal;
-    }
-
-    function test()
-    {
-        return $this->maxSatisfaction([-1, -8, 0, 5, -9]);
-//        return $this->maxSatisfaction([4, 3, 2]);
-//        return $this->maxSatisfaction([-1, -4, -5]);
+        return $result;
     }
 }
