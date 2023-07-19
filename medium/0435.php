d44ec7c59c247c9001fddcd38f9c743a666e166b
tinghaolai <analth355@gmail.com>
2023-07-19 12:55:47 +0800

    0435 update solution

--- medium/0435.php
+++ medium/0435.php
@@ -8,22 +8,16 @@ class Solution {
      */
     function eraseOverlapIntervals($intervals) {
         usort($intervals, function ($a, $b) {
-            if ($a[1] == $b[1]) {
-                return $a[0] - $b[0];
-            }
-
             return $a[1] - $b[1];
         });
 
         $result = 0;
-        $min = $intervals[0][0];
         $max = $intervals[0][1];
         $count = count($intervals);
         for ($i = 1; $i < $count; $i++) {
             $current = $intervals[$i];
-            if ($current[0] >= $max || $current[1] <= $min) {
-                $min = min($min, $current[0]);
-                $max = max($max, $current[1]);
+            if ($intervals[$i][0] >= $max) {
+                $max = $current[1];
             } else {
                 $result++;
             }
